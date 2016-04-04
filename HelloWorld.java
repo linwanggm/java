1571e4a00f6c8d167545c4b02c58a90a70cbb1dc
analyzee <linwanggm@gmail.com>
2016-04-04 10:28:22 +0800

    add enum example

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+1. HellWorld
 // my first program for java
 // platforms: Eclipse
 //filename: HelloWorld.java
@@ -19,6 +20,27 @@ hello world,JAVA.
 1. java注释同C++/C一样，"//" 和"/**/"都可以。
 2. class名字同文件名同名
 3. 该程序从public static void main(String[] args)开始执行。
+
+
+2. enum
+/*
+*/
+package javaExample;
+
+public class javaExample {
+	enum fruit {apple,orange,banana}
+	static fruit f1=fruit.orange;
+	public static void main(String[] args)
+	{
+		System.out.println(f1);
+		System.out.println(f1.getClass().toString());  //get the type of var
+	};
+}
+
+---
+orange
+class javaExample.javaExample$fruit
+
 
 
 
