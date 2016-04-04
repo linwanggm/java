241c1d25f0aa1bd61d0d7d2c17b464143d8d2bd7
analyzee <linwanggm@gmail.com>
2016-04-04 09:43:16 +0800

    my first program for java: Hello World,JAVA

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+// my first program for java
+// platforms: Eclipse
+//filename: HelloWorld.java
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hello world,JAVA.");
+	}
+
+}
+
+---
+hello world,JAVA.
+
+------
+1. java注释同C++/C一样，"//" 和"/**/"都可以。
+2. class名字同文件名同名
+3. 该程序从public static void main(String[] args)开始执行。
+
+
+
