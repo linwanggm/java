1c9fb5facf0bbd88cd656020023095c8309ea8fb
analyzee <linwanggm@gmail.com>
2016-04-08 00:04:41 +0800

    添加字符串示例

--- stream1.java
+++ stream1.java
@@ -1,5 +1,6 @@
 //learn from: http://www.runoob.com/java/java-files-io.html
 
+1. 控制台输入字符
 import java.io.*;
 
 public class BBREAD {
@@ -21,3 +22,28 @@ public class BBREAD {
 
 ----
 Enter characters, 'q' to quit.
+
+
+2. 控制台输入字符串
+import java.io.*;
+
+public class BBREAD {
+	public static void main(String args[]) throws IOException
+	   {
+	      // 使用 System.in 创建 BufferedReader 
+	      BufferedReader br = new BufferedReader(new
+	                              InputStreamReader(System.in));
+	      String str;
+	      System.out.println("Enter lines of text.");
+	      System.out.println("Enter 'end' to quit.");
+	      do {
+	         str = br.readLine();
+	         System.out.println(str);
+	      } while(!str.equals("end"));
+	   }
+}
+
+---
+Enter lines of text.
+Enter 'end' to quit.
+
